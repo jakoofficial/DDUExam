fa20cf0b11c2b3f4141b8627b95232998a516c1e
jakoofficial <jacobvoandersen@gmail.com>
2020-04-08 13:19:44 +0200

    Added new animations to text box and a blackscreen that will come when you go through a door.

--- DDUExamTitanic/Assets/Scripts/DoorMechanic.cs
+++ DDUExamTitanic/Assets/Scripts/DoorMechanic.cs
@@ -7,12 +7,14 @@ public class DoorMechanic : MonoBehaviour
 
     public Transform doorNext;
     private GameObject player;
+    private GameObject blackScreen;
 
     private bool activable = false;
 
     private void Start()
     {
         player = GameObject.FindWithTag("Player");
+        blackScreen = GameObject.FindWithTag("BlackScreen"); 
     }
 
     private void OnTriggerEnter2D(Collider2D col)
@@ -38,8 +40,19 @@ public class DoorMechanic : MonoBehaviour
     {
         if (activable && Input.GetKeyDown("e"))
         {
-            player.transform.position = new Vector2(doorNext.position.x, doorNext.position.y);
+            //player.transform.position = new Vector2(doorNext.position.x, doorNext.position.y+0.6f);
+            StopAllCoroutines();
+            StartCoroutine(BlackScreen());
         }
+    }
+
+    IEnumerator BlackScreen()
+    {
+        blackScreen.GetComponent<Animator>().SetBool("bScreenStart", true);
+        yield return new WaitForSecondsRealtime(0.3f);
+        player.transform.position = new Vector2(doorNext.position.x, doorNext.position.y + 0.6f);
+        blackScreen.GetComponent<Animator>().SetBool("bScreenStart", false);
+
     }
 
 }
