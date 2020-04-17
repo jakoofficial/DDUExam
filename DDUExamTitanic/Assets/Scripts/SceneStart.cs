0f1a5d9bec2f4fa9473238bfc2603613bede85d2
jakoofficial <jacobvoandersen@gmail.com>
2020-04-17 10:09:09 +0200

    Added a new test scene for testing days and added a new animation for the start of the day.

---
+++ DDUExamTitanic/Assets/Scripts/SceneStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneStart : MonoBehaviour
+{
+    public Animator blackscreenNewDay;
+    public Text newDayText;
+    public string newDayString;
+
+    private void Start()
+    {
+        newDayText.text = newDayString.ToString();
+        blackscreenNewDay.SetBool("newDay", true);
+        StartCoroutine(newDay());
+    }
+
+    IEnumerator newDay()
+    {
+        yield return new WaitForSeconds(1.5f);
+        blackscreenNewDay.SetBool("newDay", false);
+    }
+}
---
+++ DDUExamTitanic/Assets/Scripts/SleepingScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SleepingScript : MonoBehaviour
+{
+    public Animator blackScreen;
+
+    private bool collision = false;
+
+    private void Update()
+    {
+        if (collision && Input.GetKeyDown(KeyCode.E))
+        {
+            Sleep();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            collision = true;
+        }
+    }
+
+    public void Sleep()
+    {
+        blackScreen.SetBool("bScreenStart", true);
+        StartCoroutine(changeScene());
+    }
+
+    IEnumerator changeScene()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene("Jacob 1");
+    }
+}
