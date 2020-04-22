3d8f9391a059a3f4d4cfe6849a840b40672a3a74
jakoofficial <jacobvoandersen@gmail.com>
2020-04-22 15:52:48 +0200

    Did animation stuff

--- DDUExamTitanic/Assets/Scripts/PlayerMovement.cs
+++ DDUExamTitanic/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 4f;
     public Rigidbody2D rb;
+    public Animator anim;
 
     public bool moveable = true;
+    private bool isRight = true;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -20,11 +24,31 @@ public class PlayerMovement : MonoBehaviour
             float hAxis = Input.GetAxis("Horizontal");
             float vAxis = Input.GetAxis("Vertical");
 
+            if (hAxis > 0)
+            {
+                anim.SetBool("WalkingSides", true);
+                FlipX(false);
+            }
+            else if (hAxis < 0)
+            {
+                anim.SetBool("WalkingSides", true);
+                FlipX(true);
+            }
+            else if (hAxis == 0)
+            {
+                anim.SetBool("WalkingSides", false);
+            }
+
             rb.velocity = new Vector2(hAxis * speed, vAxis * speed);
         }
         else
         {
             rb.velocity = Vector2.zero;
         }
+    }
+
+    public void FlipX(bool flip)
+    {
+        this.GetComponent<SpriteRenderer>().flipX = flip;
     }
 }
